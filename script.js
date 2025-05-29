b6ff424d58b71d62163e8a1681813ed7989e0d9f
Eliška Štěpánová <stepanova.eliska01@gmail.com>
2025-05-29 23:43:57 +0200

    Úprava úvodní strany. K předešlému commitu doplňuji přidání fontu do page O projektu a lehká stylistická úprava textu, plus výměna obrázku.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+const grid = document.querySelector('#grid');
+
+for (let i = 0; i < 9; i++) {
+  const cell = document.createElement('div');
+  cell.classList.add('cell');
+  grid.appendChild(cell);
+}
