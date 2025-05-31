4334b55edfa153dbcae31085f80b9b815b0dcdd6
Zuzana Ilečková <yes>
2025-06-01 01:32:21 +0200

    vymazání zakomentovaných nechtěných částí kódu

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/*const grid = document.querySelector('#grid');
-
-for (let i = 0; i < 9; i++) {
-  const cell = document.createElement('div');
-  cell.classList.add('cell');
-  grid.appendChild(cell);
-}*/
-
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('navMenu');
 
