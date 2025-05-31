075dfca004d39eb527f7102def78d3fd470725ee
Zuzana Ilečková <yes>
2025-05-31 16:26:34 +0200

    úprava hlavičky a navigace, nastaveni hamburger menu v ramci responzivity

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-
-const grid = document.querySelector('#grid');
+/*const grid = document.querySelector('#grid');
 
 for (let i = 0; i < 9; i++) {
   const cell = document.createElement('div');
   cell.classList.add('cell');
   grid.appendChild(cell);
-}
+}*/
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
